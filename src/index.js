cd603c291b96c24480514133e564cfb2aa343ffe
Michal K <michal10203040@gmail.com>
2019-09-30 10:58:47 +0200

    Redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducet from './reducers/rootReducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducet);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 import socket from "./ws";
 
